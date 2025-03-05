028c7046f10a919a83ba82cd0a809c6ece7f7bf2
d-hooper <dallinhoop3@gmail.com>
2025-03-05 14:54:34 -0700

    drawing pokemon list to view

--- app/controllers/PokemonController.js
+++ app/controllers/PokemonController.js
@@ -1,10 +1,20 @@
+import { AppState } from "../AppState.js"
 import { pokemonService } from "../services/PokemonService.js"
 import { Pop } from "../utils/Pop.js"
 
 export class PokemonController {
 
   constructor() {
+    AppState.on('pokemon', this.drawPokemon)
     this.getPokemon()
+  }
+
+  drawPokemon() {
+    const pokemon = AppState.pokemon
+    let pokemonContent = ''
+    const pokeListElm = document.getElementById('pokeList')
+    pokemon.forEach(poke => pokemonContent += poke.pokemonList)
+    pokeListElm.innerHTML = pokemonContent
   }
 
   async getPokemon() {
--- app/models/Pokemon.js
+++ app/models/Pokemon.js
@@ -5,4 +5,13 @@ export class Pokemon {
   constructor(data) {
     this.name = data.name
   }
+
+  get pokemonList() {
+    return `
+    <button class="btn btn-dark text-teal text-capitalize text-start fw-bold w-100">
+      <span class="mdi mdi-pokeball"></span>
+      ${this.name}
+    </button>
+    `
+  }
 }
